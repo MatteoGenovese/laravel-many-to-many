993f038d62b9a880b61abe748362e229c2d31cc6
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-22 17:19:36 +0200

    feat: new resource for CategoryController

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware('auth')
     ->group(function (){
             Route::get('/', 'HomeController@index')->name('home');
             Route::resource('/posts', 'PostController');
+            Route::resource('/categories', 'CategoryController');
 
 });
-
 // Route::middleware('auth')->resource('/posts', 'PostsController');
