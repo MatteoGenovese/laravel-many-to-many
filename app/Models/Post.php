cb8d4fcd6b231c6e639d48131e6faf5dc13e401c
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-22 16:15:43 +0200

    blank

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'user_id',
+        'post_id',
+        'post_date',
+        'post_image',
+        'post_content'
+    ];
+
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
+    public function category() {
+        return $this->belongsTo('App\Models\Category');
+    }
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //
+
+        $users= User::all();
+        $categories = Category::all();
+
+        for ($i=0; $i < 100; $i++) {
+            $newPost = new Post();
+            $newPost->user_id = $faker->randomElement($users)->id;
+            $newPost->category_id = $faker->randomElement($categories)->id;
+            $newPost->title = $faker->realText(35);
+            // $newPost->author = $newPost->user->name;
+            $newPost->post_date = $faker->date('Y-m-d');
+            $newPost->post_image = $faker->imageUrl();
+            $newPost->post_content = $faker->paragraphs(5, true);
+            $newPost->save();
+        }
+    }
+}
