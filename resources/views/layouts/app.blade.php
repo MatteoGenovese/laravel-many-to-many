752b2b6559577ffa1cdc48f7ea94fe9f32637480
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-22 17:19:07 +0200

    feat: new link on header Categories List

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,9 @@
                 <a class="navbar-brand" href="{{ route('admin.posts.index') }}">
                     Post List
                 </a>
+                <a class="navbar-brand" href="{{ route('admin.categories.index') }}">
+                    Categories List
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
