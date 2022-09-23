d26f962342acd290eb71babbdec6a22b3f35cb5b
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-23 14:44:30 +0200

    style: new template

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,42 +1,50 @@
 @extends('layouts.app')
 
+@php
+$date = $post->post_date;
+$newDate = explode(' ', $date);
+@endphp
 
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-12">{{ $post->id }}</div>
-        <div class="col-4">{{ $post->title }}</div>
-        <div class="col-4">{{ $post->author }}</div>
-        @php
-            $date = $post->post_date;
-            $newDate = explode(' ', $date);
-        @endphp
-        <div class="col-4">{{
-            $newDate[0]
-        }}</div>
-        <div class="col-4"><img src="{{ $post->post_image }}" class="w-100" alt="{{ $post->title }} image"></div>
-        <div class="col-4">{{ $post->post_content }}</div>
-        <div class="col-4">
 
-            <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-sm btn-success">
-                Edit
-            </a>
-
-            <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger" value="delete">
-                    destroy
-                </button>
-            </form>
-
+        <div class="col-6">
+            <div class="row">
+                <div class="col-12"><img src="{{ $post->post_image }}" class="w-100" alt="{{ $post->title }} image"></div>
+                <div class="col-12">{{ $post->title }}</div>
+            </div>
         </div>
-        <div class="col-4">
-            @forelse ($post->tags as $tag)
-                {{ '#'.$tag->name." " }}
-            @empty
 
-            @endforelse
+        <div class="col-6">
+
+            <div class="row">
+                <div class="col-12">{{ $post->post_content }}</div>
+            </div>
+
+            <div class="row mt-4">
+                <div class="col">{{ $post->user->name }}</div>
+                <div class="col">{{ $newDate[0] }}</div>
+                <div class="col">
+                    @forelse ($post->tags as $tag)
+                        {{ '#'.$tag->name." " }}
+                    @empty
+                        {{ 'No tags..' }}
+                    @endforelse
+                </div>
+                <div class="col d-inline">
+                    <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-sm btn-success">
+                        Edit
+                    </a>
+                    <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger" value="delete">
+                            destroy
+                        </button>
+                    </form>
+                </div>
+            </div>
 
         </div>
     </div>
