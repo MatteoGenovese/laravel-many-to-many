c32d71604eecc3ac942274d983111c053979dd4f
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-23 09:38:11 +0200

    feat: tags relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,4 +21,7 @@ class Post extends Model
     public function category() {
         return $this->belongsTo('App\Models\Category');
     }
+    public function tags(){
+        return $this->belongsToMany('App\Models\Tag');
+    }
 }
