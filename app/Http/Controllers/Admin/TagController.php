0e3dc8ad4a949ab5cad2828ea0ae2336516ba455
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-23 17:57:38 +0200

    init: tag controller

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags = Tag::all();
+        return view('admin.tags.index', compact('tags'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $sentData = $request->all();
+        $newTag = new Tag();
+        $newTag->create($sentData);
+        return redirect()->route('admin.tags.index',compact('newTag'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tag = Tag::findOrFail($id);
+        return view('admin.tags.show', compact('tag'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tag = Tag::findOrFail($id);
+        return view('admin.tags.edit', compact('tag'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $sentData = $request->all();
+        $tag = Tag::findOrFail($id);
+        $tag->update($sentData);
+        return redirect()->route('admin.tags.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $tag = Tag::where('id',$id)->first();
+        Tag::destroy($tag->id);
+        return redirect()->route('admin.tags.index');
+    }
+}
